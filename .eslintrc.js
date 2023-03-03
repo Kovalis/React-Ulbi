c14fb48cdc41adcdf649563639342ae4feafbc6f
Igor Kovalis <igor_kovalis@rambler.ru>
2023-03-03 17:52:30 +0300

    add slylelint, i18next

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,31 @@
 module.exports = {
     env: {
         browser: true,
-        es2021: true
+        es2021: true,
     },
-    extends: ['plugin:react/recommended', 'standard-with-typescript'],
-    overrides: [],
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+        'plugin:i18next/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
     parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
         ecmaVersion: 'latest',
-        sourceType: 'module'
+        sourceType: 'module',
     },
-    plugins: ['react'],
+    plugins: [
+        'react',
+        '@typescript-eslint',
+        'i18next',
+    ],
     rules: {
         'react/jsx-indent': [2, 4],
+        'react/jsx-indent-props': [2, 4],
         indent: [2, 4],
-        'react/jsx-filename-extension': [2, { extensions: ['.js', ',jsx', '.tsx'] }],
+        'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
         'import/no-unresolved': 'off',
         'import/prefer-default-export': 'off',
         'no-unused-vars': 'warn',
@@ -22,10 +34,12 @@ module.exports = {
         'react/jsx-props-no-spreading': 'warn',
         'react/function-component-definition': 'off',
         'no-shadow': 'off',
+        'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
-        'no-underscore-dangle': 'off'
+        'no-underscore-dangle': 'off',
+        'i18next/no-literal-string': ['error', { markupOnly: true }],
     },
     globals: {
-        __IS_DEV__: true
-    }
-}
+        __IS_DEV__: true,
+    },
+};
